f90fd4aad49a4c80e50eb9c0f3df3be5a7d0a2f8
githubemailacc1 <147047103+githubemailacc1@users.noreply.github.com>
2023-12-05 03:34:35 +0100

    added pre build

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Function to install dependencies based on lock file existence
+install_dependencies() {
+  if [ -f "yarn.lock" ]; then
+    # Check if Yarn is installed
+    if ! command -v yarn &>/dev/null; then
+        echo "Yarn is not installed. Installing Yarn..."
+        npm install -g yarn
+    fi
+    echo "yarn.lock found. Running yarn install..."
+    yarn install
+  else
+    echo "yarn.lock not found. Running npm install..."
+    npm install
+  fi
+}
+
+
+# Attempt to install dependencies after ensuring Yarn is available
+install_dependencies
